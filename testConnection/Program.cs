8d583045e4eb6cd4a8e2758bd0a7dbb91247b180
Inayah Mufidah <inayah.mufidah.2015@ft.umy.ac.id>
2021-03-22 11:41:37 +0700

    ubah keterangan berhasil

---
+++ testConnection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().koneksi();
+        }
+        public void koneksi()
+        {
+            using (
+                //membuat koneksi
+                SqlConnection conn = new SqlConnection("data source=DESKTOP-GVR4E5T\\INAYAH;"
+                + "database=ProdiTI;user ID=sa;password=123"))
+            {
+                conn.Open();
+                Console.Write("Koneksi Sukses");
+                Console.Read();
+            }
+        }
+    }
+}
